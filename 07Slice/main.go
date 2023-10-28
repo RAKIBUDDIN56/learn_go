a558f4de7cc841b0fd7e0e9ec36ac5b7d58557fa
Rakib Uddin <rsrakib404@gmail.com>
2023-10-28 22:03:11 +0600

    remove item using index form slices

--- 07Slice/main.go
+++ 07Slice/main.go
@@ -8,40 +8,40 @@ import (
 func main() {
 	fmt.Println("Welcome to Slices ")
 
-	var fruitList =[] string{"Apple","Banana","Orange"}
-	fmt.Printf("Tyoe of fruitList is %T\n",fruitList)
-	var vegList =[3]string{"potato","beans","mushroom"}
-	fmt.Printf("Veg List %T",vegList)
-	fruitList=append(fruitList,"Pineapple" )
+	var fruitList = []string{"Apple", "Banana", "Orange"}
+	fmt.Printf("Tyoe of fruitList is %T\n", fruitList)
+	var vegList = [3]string{"potato", "beans", "mushroom"}
+	fmt.Printf("Veg List %T", vegList)
+	fruitList = append(fruitList, "Pineapple")
 	fmt.Println(fruitList)
-a:= [] string {"A"}
-a= append(a,fruitList...)
-fmt.Println(len(a))
-//create slice form array
-// slice := array[start_index : end_index]
-arr1 :=[6] int {10, 11, 12, 13, 14,15}
-mySlice := arr1[2:4]
+	a := []string{"A"}
+	a = append(a, fruitList...)
+	fmt.Println(len(a))
+	//create slice form array
+	// slice := array[start_index : end_index]
+	arr1 := [6]int{10, 11, 12, 13, 14, 15}
+	mySlice := arr1[2:4]
 
+	fmt.Println(mySlice)
 
-fmt.Println(mySlice)
-
-//creat slice using make() function
-newSlice :=make([] int,4)
-newSlice[0]=2;
-newSlice[1]=45;
-newSlice[2]=3;
-newSlice[3]=10;
-newSlice=append(newSlice,10)
-fmt.Println(cap(newSlice))
-fruitList=fruitList[1:2] 
-fmt.Println(fruitList)
-sort.Ints(newSlice)
-fmt.Println(newSlice)
-fmt.Println(sort.IntsAreSorted(newSlice))
-
-
-
-	
-
+	//creat slice using make() function
+	newSlice := make([]int, 4)
+	newSlice[0] = 2
+	newSlice[1] = 45
+	newSlice[2] = 3
+	newSlice[3] = 10
+	newSlice = append(newSlice, 10)
+	fmt.Println(cap(newSlice))
+	fruitList = fruitList[1:2]
+	fmt.Println(fruitList)
+	sort.Ints(newSlice)
+	fmt.Println(newSlice)
+	fmt.Println(sort.IntsAreSorted(newSlice))
+	//remove a value from slices based on index
+	var courses =[]string{"reactjs","js","go","python"}
+	fmt.Println(courses)
+	var index int =1;
+	courses=append(courses[:index],courses[index+1:]...)
+	fmt.Println(courses)
 
 }
